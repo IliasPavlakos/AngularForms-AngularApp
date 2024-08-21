d7232cd82399682ca7d1911df6e8a63dc1096d49
ipavlakos <pavlakosIlias@outlook.com>
2024-08-21 20:31:37 +0300

    Template-driven: Interacting with the underlying form object in the component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
+import {debounceTime, pipe} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,18 @@ import {FormsModule, NgForm} from "@angular/forms";
 })
 export class LoginComponent {
 
+  private form = viewChild<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form()?.valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('save-login-form', JSON.stringify({email:value.email})),
+      });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData : NgForm){
 
     if(!formData.valid){
@@ -22,6 +35,8 @@ export class LoginComponent {
     const enteredPassword = formData.form.value.password;
     console.log(formData);
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
   }
 
 }
